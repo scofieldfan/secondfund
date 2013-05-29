c47f1302a40ad3c8c846d1bb659923204f9a4053
fan zhang <scofieldfan@163.com>
2013-05-29 14:03:20 +0800

    20130529 add crawl data

--- handler.py
+++ handler.py
@@ -1,8 +1,10 @@
 # _*_ coding:gbk _*_
 import os 
 import crawl
+import urllib2
 list =  os.listdir("./data")
 crawl.test()
+myUrl='http://secondhandhouse.duapp.com/insert.jsp?fundate=%s&num=%s'
 for f in list:
 	 fl = file("./data/"+f);
 	 crawl.log("handler the file %s " % f)
@@ -11,5 +13,12 @@ for f in list:
 	 crawl.log(yesStr)
 	 #yesStr = crawl.getYesterdayStr()
 	 sn = crawl.getSecondSellNumber(yesStr,html)
-       	 crawl.insertToDb(yesStr,sn)
+         url_final = myUrl % (yesStr,sn);
+	 print url_final
+	 try:
+		req = urllib2.Request(url_final)
+	       	html=urllib2.urlopen(req).read().decode("utf-8").encode("gb18030")
+	 except:
+		print "error"
+       	 #crawl.insertToDb(yesStr,sn)
 	 
