5500020b37d58e07d68b9ccc27e2a4bc0e2a23ec
afhs324 <scofieldfan@163.com>
2013-06-09 11:27:08 +0800

    提交代码

--- fetchPigPrice.py
+++ fetchPigPrice.py
@@ -2,7 +2,6 @@
 import urllib
 import urllib2
 import datetime
-import MySQLdb
 
 myUrl='http://www.zhujiage.com.cn/Article/showlist.php?tid=24&TotalResult=38533&PageNo=%s'
 def test():
@@ -15,19 +14,30 @@ def insertToDb(dateStr,sellNo):
 	cur.execute(sqlStr);
 	return sqlStr;
 
-def getSecondSellNumber(html):
+def getSubString(html,startTag,endTag):
 	indexS = html.find(startTag);	
 	if(indexS!=-1):
 		indexE=html.find(endTag,indexS)
 		if(indexE!=-1 and indexE>indexS):
 			start=indexS+len(startTag)
-			sellNumber=html[start:indexE]
-			log("%s second fund sell no is %s" % (yestStr,sellNumber))
-			return sellNumber
-def writeFile(filename)
-	f = file(filename,'w')
+			result=html[start:indexE]
+			return result
+def getSubStringList(html,startTag,endTag):
+	result=[];
+	indexS = html.find(startTag);	
+	while(indexS!=-1):
+		indexE=html.find(endTag,indexS)
+		if(indexE!=-1 and indexE>indexS):
+			start=indexS
+			end=indexE+len(endTag)
+			tagResult = html[start:end]
+			result.append(tagResult)
+			indexS = html.find(startTag,indexE);
+	return result;
+def writeFile(filename,content):
+	f = file(filename,'a')
 	log("write file "+ filename)
-	f.write(html)
+	f.write(''.join(content))
 	f.close
 
 def getYesterdayStr():
@@ -39,9 +49,18 @@ def log(sth):
 	print nowStr+sth
 if __name__ == "__main__":
 	simpleList=[]
-	for i in range(1,10):
+	for i in range(1,714):
 		url = myUrl % i
 		log("begin crawl "+ url)
 		req= urllib2.Request(url)
 		html=urllib2.urlopen(req).read()
+		listHtml =  getSubString(html,'<div class="lmlist">','<div class="showpage">');
+		resultList= getSubStringList(listHtml,'<A','</A>')
+		for re in resultList:
+			if re.find('山东')!=-1:
+				log(re)
+				simpleList.append(re+"\n")
+		writeFile("山东PigPrice.out",simpleList)
+		
+
 		
