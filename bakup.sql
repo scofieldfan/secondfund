344eeb93b625b8c87249c8893354a4baf0d6ed9d
fan zhang <scofieldfan@163.com>
2013-07-21 07:40:03 +0800

    20130721 add crawl data

--- bakup.sql
+++ bakup.sql
@@ -867,3 +867,4 @@ insert into secondfund values ('20130716','480');
 insert into secondfund values ('20130717','375');
 insert into secondfund values ('20130718','340');
 insert into secondfund values ('20130719','477');
+insert into secondfund values ('20130720','478');
